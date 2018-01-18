bc095a40dd91871d9811f4729a6bac487901320d
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-01-17 23:37:19 -0800

    use gem Faraday to fetch daily image url and explanation and include them in message body. Also only reply to 'space me' request

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,14 +3,17 @@ class MessagesController < ApplicationController
 
   def reply
     message = params["Body"]
-    from = params["From"]
-    twilio
-    mms = @client.messages.create(
-      body: "Here is your photo of the day!",
-      from: Rails.application.secrets.twilio_number,
-      to: from,
-      media_url: "https://api.nasa.gov/images/apod.jpg"
-    )
+    if message["space me"] || message["SPACE ME"]
+      from = params["From"]
+      get_nasa_url
+      twilio
+      mms = @client.messages.create(
+        body: @explanation,
+        from: Rails.application.secrets.twilio_number,
+        to: from,
+        media_url: @url
+      )
+    end
   end
  
   private
@@ -19,5 +22,14 @@ class MessagesController < ApplicationController
     twilio_sid = Rails.application.secrets.twilio_sid
     twilio_token = Rails.application.secrets.twilio_token
     @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
+  end
+
+  def get_nasa_url
+    nasa_key = Rails.application.secrets.nasa_api
+    nasa_response = Faraday.get("https://api.nasa.gov/planetary/apod?api_key=#{nasa_key}")
+    body_hash = JSON.parse(nasa_response.body)
+    @url = body_hash['url']
+    @explanation = body_hash['explanation']
+
   end
 end
